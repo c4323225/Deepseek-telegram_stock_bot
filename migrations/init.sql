20671a5e89654f7d00b50ed9ab442fe4509d4e32
VickySingh <c4323225@gmail.com>
2025-07-12 14:10:16 +0530

    Update init.sql

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,1 +1,13 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT,
+    referral_count INTEGER DEFAULT 0,
+    invited_by INTEGER DEFAULT 0
+);
 
+CREATE TABLE referrals (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    referrer_id INTEGER,
+    referred_id INTEGER,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
