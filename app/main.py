65dd57fe4d401456eefe2e953a57fc48092acd76
VickySingh <c4323225@gmail.com>
2025-07-12 14:11:00 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 import logging
 import asyncio
 from dotenv import load_dotenv
-from bot.handlers import setup_handlers
 from telegram.ext import Application
+from app.bot.handlers import setup_handlers
+import os
 
 load_dotenv()
 
 async def main():
-    app = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
-    await setup_handlers(app)
-    await app.run_polling()
+    application = Application.builder().token(os.getenv("TELEGRAM_TOKEN")).build()
+    setup_handlers(application)
+    await application.run_polling()
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
