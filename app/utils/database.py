143c359aa5bb9a883c679730c9cb4029b7d3f431
VickySingh <c4323225@gmail.com>
2025-07-12 14:01:51 +0530

    Update database.py

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,1 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
 
+engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
